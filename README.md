9e6e8ad9aa4d9a4bffec39be694f7528cfda6bed
GeethaCharan31 <geethacharan31@gmail.com>
2021-06-09 11:20:35 +0530

    Added README file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mail Client ![image](images/email.png)
+A simple mail client to send mails either through voice commands or conventional typing.
--- mail.py
+++ mail.py
@@ -1,12 +1,12 @@
 import smtplib
 from email.message import EmailMessage
 
-def sendmail(mail, pswd, sendTo, subject, message):
+def sendmail(mailId, password, sendTo, subject, message):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login(mail, pswd)
+    server.login(mailId, password)
     email=EmailMessage()
-    email["From"]=mail
+    email["From"]=mailId
     email["To"]=sendTo
     email["Subject"]=subject
     email.set_content(message)
